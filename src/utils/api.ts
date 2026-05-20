c74c372eacfc17fff24ad695506708c2d247dae6
Andres Gonzalez <afgquinones@gmail.com>
2026-05-20 16:25:07 -0500

    config(api): hardcode production gateway URL temporarily

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: (import.meta.env.VITE_API_BASE_URL as string) || 'http://localhost:8080',
+    baseURL: 'http://158.247.122.168:8080', // TODO: migrate to VITE_API_BASE_URL env var
     headers: {
         'Content-Type': 'application/json',
     },
